053b6b92805f6f9f58dbec3d78c56cff006e285d
Mirko <mirko.longo.1987@gmail.com>
2020-04-20 16:50:02 +0200

    Inizio Header

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
         <title>Ajax Dischi</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="dist/css/style.css">
     </head>
     <body>
       <header>
-
+        <div class="header-box-img">
+          <a href="index.php"><img src="https://pluspng.com/img-png/spotify-logo-png-icons-logos-emojis-tech-companies-2000.png" alt="https://pluspng.com/img-png/spotify-logo-png-icons-logos-emojis-tech-companies-2000.png"></a>
+        </div>
       </header>
       <main>
 
