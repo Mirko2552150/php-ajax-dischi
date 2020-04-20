dbbadac9fe20ae2e5e4f16a39191896861ff8b7e
Mirko <mirko.longo.1987@gmail.com>
2020-04-20 15:56:14 +0200

    Inizio creazione file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Ajax Dischi</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+      <script src="js/main.js" charset="utf-8"></script>
+    </body>
+</html>
