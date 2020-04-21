11c5b2c0f5fc1277095379e0624446ad5c290563
Mirko <mirko.longo.1987@gmail.com>
2020-04-21 11:50:04 +0200

    Inserimento DATA.PHP

--- data.php
+++ data.php
@@ -4,37 +4,37 @@
       'title' => 'Titolo Uno',
       'artist' => 'Queen Uno',
       'year' => '1999',
-      'img' => '_uno',
+      'img' => '_uno'
     ],
     [
       'title' => 'Titolo Due',
       'artist' => 'Queen Due',
       'year' => '2000',
-      'img' => '_due',
+      'img' => '_due'
     ],
     [
       'title' => 'Titolo Tre',
       'artist' => 'Queen Tre',
       'year' => '2001',
-      'img' => '_tre',
+      'img' => '_tre'
     ],
     [
       'title' => 'Titolo Quattro',
       'artist' => 'Queen Quattro',
       'year' => '2002',
-      'img' => '_quattro',
+      'img' => '_quattro'
     ],
     [
       'title' => 'Titolo Cinque',
       'artist' => 'Queen Cinque',
       'year' => '2003',
-      'img' => '_cinque',
+      'img' => '_cinque'
     ],
     [
       'title' => 'Titolo Sei',
       'artist' => 'Queen Sei',
       'year' => '2004',
-      'img' => '_sei',
+      'img' => '_sei'
     ]
   ]
 
--- indexArrayDentroPHP.php
+++ indexArrayDentroPHP.php
@@ -1,6 +1,44 @@
 
 <?php
-  include 'data.php'; // inserisoc un file esterno con i miei dati PHP
+// UTILIZZO PHP DENTRO IL FILE INDEX
+  $albums = [
+    [
+      'title' => 'Titolo Uno',
+      'artist' => 'Queen Uno',
+      'year' => '1999',
+      'img' => '_uno',
+    ],
+    [
+      'title' => 'Titolo Due',
+      'artist' => 'Queen Due',
+      'year' => '2000',
+      'img' => '_due',
+    ],
+    [
+      'title' => 'Titolo Tre',
+      'artist' => 'Queen Tre',
+      'year' => '2001',
+      'img' => '_tre',
+    ],
+    [
+      'title' => 'Titolo Quattro',
+      'artist' => 'Queen Quattro',
+      'year' => '2002',
+      'img' => '_quattro',
+    ],
+    [
+      'title' => 'Titolo Cinque',
+      'artist' => 'Queen Cinque',
+      'year' => '2003',
+      'img' => '_cinque',
+    ],
+    [
+      'title' => 'Titolo Sei',
+      'artist' => 'Queen Sei',
+      'year' => '2004',
+      'img' => '_sei',
+    ],]
+
 ?>
 
 <!DOCTYPE html>
@@ -36,7 +74,7 @@
                    <p><span>Anno: </span><span><?php echo $value[year] ?></span></p>
                  </div>
                </div>
-          
+             <?php endforeach; ?>
             <!-- <div class="card">
               <div class="card-img">
                 <img src="img/album_uno.jpg" alt="img/album_uno">
