ff0a5ba1ef331450d66cebe8eece53b993c087db
Mirko <mirko.longo.1987@gmail.com>
2020-04-21 11:20:50 +0200

    Inserimento PHP inizio

---
+++ data.php
@@ -0,0 +1,43 @@
+<?php
+  $albums = [
+    [
+      'title' => 'Titolo Uno',
+      'artist' => 'Queen Uno',
+      'year' => '1999',
+      'img' => '_uno',
+    ],
+    [
+      'title' => 'Titolo Due',
+      'artist' => 'Queen Due',
+      'year' => '2000',
+      'img' => '_due',
+    ],
+    [
+      'title' => 'Titolo Tre',
+      'artist' => 'Queen Tre',
+      'year' => '2001',
+      'img' => '_tre',
+    ],
+    [
+      'title' => 'Titolo Quattro',
+      'artist' => 'Queen Quattro',
+      'year' => '2002',
+      'img' => '_quattro',
+    ],
+    [
+      'title' => 'Titolo Cinque',
+      'artist' => 'Queen Cinque',
+      'year' => '2003',
+      'img' => '_cinque',
+    ],
+    [
+      'title' => 'Titolo Sei',
+      'artist' => 'Queen Sei',
+      'year' => '2004',
+      'img' => '_sei',
+    ]
+  ]
+
+
+
+ ?>
--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+  $albums = [
+    [
+      'title' => 'Titolo Uno',
+      'artist' => 'Queen Uno',
+      'year' => '1999',
+      'img' => '_uno',
+    ],
+    [
+      'title' => 'Titolo Due',
+      'artist' => 'Queen Due',
+      'year' => '2000',
+      'img' => '_due',
+    ],
+    [
+      'title' => 'Titolo Tre',
+      'artist' => 'Queen Tre',
+      'year' => '2001',
+      'img' => '_tre',
+    ],
+    [
+      'title' => 'Titolo Quattro',
+      'artist' => 'Queen Quattro',
+      'year' => '2002',
+      'img' => '_quattro',
+    ],
+    [
+      'title' => 'Titolo Cinque',
+      'artist' => 'Queen Cinque',
+      'year' => '2003',
+      'img' => '_cinque',
+    ],
+    [
+      'title' => 'Titolo Sei',
+      'artist' => 'Queen Sei',
+      'year' => '2004',
+      'img' => '_sei',
+    ],
+  ]
+  ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -325,12 +366,12 @@
       <script id="template-card" type="text/x-handlebars-template">
         <div class="card">
           <div class="card-img">
-            <img src="img/album_uno.jpg" alt="img/album_uno">
+            <img src="img/album_{{cover}}" alt="img/album_uno">
           </div>
           <div class="card-description">
-            <p><span>Titolo Album: </span><span>AAAA</span></p>
-            <p><span>Artista: </span><span></span>Queen</p>
-            <p><span>Anno: </span><span>1999</span></p>
+            <p><span>Titolo Album: </span><span>{{titolo}}</span></p>
+            <p><span>Artista: </span><span></span>{{Artista}}</p>
+            <p><span>Anno: </span><span>{{anno}}</span></p>
           </div>
         </div>
       </script>
